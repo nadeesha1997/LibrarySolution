15ffb86ee8d4205f04eb5c9980cb316c276e4c03
nadeesha97 <nadeeshamadhushan9@gmail.com>
2020-07-11 20:19:36 +0530

    Add project files.

---
+++ LibrarySystem/Controllers/BookController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibrarySystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LibrarySystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public BookController()
+        {
+            Database.Add(new Book { id=1, Name = "Book1", Auther = "Auther1" });
+            Database.Add(new Book { id = 2, Name = "Book2", Auther = "Auther2" });
+        }
+        List<Book> Database = new List<Book>();
+        // GET: api/<BookController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return Database;
+        }
+
+        // GET api/<BookController>/5
+        [HttpGet("{id}")]
+        public Book Get(int id)
+        {
+            return new Book { id = 1, Name = "book1", Auther = "auther"};
+
+        }
+
+        // POST api/<BookController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BookController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BookController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
